06f1ca6242fe129cff5eb8b0d5cac344d5faaba4
50616003 <50616003@50616003-pc>
2017-12-11 17:40:35 +0900

    FrameTest

---
+++ src/frametest/Frame_main.java
@@ -0,0 +1,10 @@
+package frametest;
+
+public class Frame_main {
+
+	public static void main(String[] args) {
+		Frame_view frame = new Frame_view();
+		frame.setBounds(5,5,655,455);
+		frame.setVisible(true);
+	}
+}
